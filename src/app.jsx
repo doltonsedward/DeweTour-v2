33670391b4d397b67688449ab5738bf47770da49
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-01 22:39:08 +0800

    init preact & vite

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import { Logo } from "./logo";
+import Header from "./components/Header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  );
+}
