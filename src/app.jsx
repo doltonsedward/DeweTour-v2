8e3b4ba469c0c6ce0185ca4919a11cd382988aa1
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-02 00:09:06 +0800

    testing for Material ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,10 @@
-import { Logo } from "./logo";
-import Header from "./components/Header";
+import Router from "preact-router";
+import Home from "./presentation/pages/Home";
 
-export function App() {
-  return (
-    <>
-      <Header />
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Home path="/" />
+  </Router>
+);
+
+export default App;
---
+++ src/presentation/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Typography>Hello Vitessss!</Typography>
+    </>
+  );
+};
+
+export default Home;
